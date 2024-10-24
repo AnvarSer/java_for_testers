fd2f18ba4babe505efd7b20ec076695bf1427512
Anvar <myanvar@yahoo.com>
2024-10-24 17:23:37 +0600

    Переработанное домашнее задание 4

--- sandbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
@@ -2,11 +2,10 @@ package ru.stqa.geometry.figures;
 
 import java.util.Objects;
 
-public record Triangle (
+public record Triangle(
         double a,
         double b,
-        double c)
-{
+        double c) {
 
     public Triangle {
         if (a < 0 || b < 0 || c < 0 || a + b < c || a + c < b || b + c < a) {
@@ -32,22 +31,77 @@ public record Triangle (
         return Math.sqrt(halfPerimeter * (halfPerimeter - a) * (halfPerimeter - b) * (halfPerimeter - c));
     }
 
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Triangle triangle = (Triangle) o;
-        return (Double.compare(triangle.a, this.a) == 0 && Double.compare(triangle.b, this.b) == 0 && Double.compare(triangle.c, this.c) == 0)
-                || (Double.compare(triangle.a, this.a) == 0 && Double.compare(triangle.b, this.c) == 0 && Double.compare(triangle.c, this.a) == 0)
-                || (Double.compare(triangle.a, this.b) == 0 && Double.compare(triangle.b, this.c) == 0 && Double.compare(triangle.c, this.a) == 0)
-                || (Double.compare(triangle.a, this.b) == 0 && Double.compare(triangle.b, this.a) == 0 && Double.compare(triangle.c, this.b) == 0)
-                || (Double.compare(triangle.a, this.c) == 0 && Double.compare(triangle.b, this.a) == 0 && Double.compare(triangle.c, this.b) == 0)
-                || (Double.compare(triangle.a, this.c) == 0 && Double.compare(triangle.b, this.b) == 0 && Double.compare(triangle.c, this.a) == 0);
+
+        double minTriangle1, midTriangle1, maxTriangle1;
+
+        if (a <= b && a <= c) {
+            minTriangle1 = a;
+        } else if (b <= a && b <= c) {
+            minTriangle1 = b;
+        } else {
+            minTriangle1 = c;
+        }
+
+        if (a >= b && a >= c) {
+            maxTriangle1 = a;
+        } else if (b >= a && b >= c) {
+            maxTriangle1 = b;
+        } else {
+            maxTriangle1 = c;
+        }
+
+        if ((a != minTriangle1) && (a != maxTriangle1)) {
+            midTriangle1 = a;
+        } else if ((b != minTriangle1) && (b != maxTriangle1)) {
+            midTriangle1 = b;
+        } else {
+            midTriangle1 = c;
+        }
+
+        double minTriangle2, midTriangle2, maxTriangle2;
+
+        if (triangle.a <= triangle.b && triangle.a <= triangle.c) {
+            minTriangle2 = triangle.a;
+        } else if (triangle.b <= triangle.a && triangle.b <= triangle.c) {
+            minTriangle2 = triangle.b;
+        } else {
+            minTriangle2 = triangle.c;
+        }
+
+        if (triangle.a >= triangle.b && triangle.a >= triangle.c) {
+            maxTriangle2 = triangle.a;
+        } else if (triangle.b >= triangle.a && triangle.b >= triangle.c) {
+            maxTriangle2 = triangle.b;
+        } else {
+            maxTriangle2 = triangle.c;
+        }
+
+        if ((triangle.a != minTriangle2) && (triangle.a != maxTriangle2)) {
+            midTriangle2 = triangle.a;
+        } else if ((triangle.b != minTriangle2) && (triangle.b != maxTriangle2)) {
+            midTriangle2 = triangle.b;
+        } else {
+            midTriangle2 = triangle.c;
+        }
+
+        return Double.compare(minTriangle1, minTriangle2) == 0 && Double.compare(midTriangle1, midTriangle2) == 0 && Double.compare(maxTriangle1, maxTriangle2) == 0;
     }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(a, b, c);
+//        return (Double.compare(triangle.a, this.a) == 0 && Double.compare(triangle.b, this.b) == 0 && Double.compare(triangle.c, this.c) == 0)
+//                || (Double.compare(triangle.a, this.a) == 0 && Double.compare(triangle.b, this.c) == 0 && Double.compare(triangle.c, this.a) == 0)
+//                || (Double.compare(triangle.a, this.b) == 0 && Double.compare(triangle.b, this.c) == 0 && Double.compare(triangle.c, this.a) == 0)
+//                || (Double.compare(triangle.a, this.b) == 0 && Double.compare(triangle.b, this.a) == 0 && Double.compare(triangle.c, this.b) == 0)
+//                || (Double.compare(triangle.a, this.c) == 0 && Double.compare(triangle.b, this.a) == 0 && Double.compare(triangle.c, this.b) == 0)
+//                || (Double.compare(triangle.a, this.c) == 0 && Double.compare(triangle.b, this.b) == 0 && Double.compare(triangle.c, this.a) == 0);
+//    }
+
+        @Override
+        public int hashCode () {
+            return 1;
+        }
     }
-}
