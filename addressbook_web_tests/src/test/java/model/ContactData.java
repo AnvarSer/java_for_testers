fa55e06540bda319b245d471dc575b8685c06a7e
Anvar <myanvar@yahoo.com>
2024-10-27 22:32:18 +0600

    Создал ContactData, ContactCreationTests, ContactRemovalTests с информацией в TestBase

--- addressbook_web_tests/src/test/java/model/ContactData.java
+++ addressbook_web_tests/src/test/java/model/ContactData.java
@@ -1,25 +1,8 @@
 package model;
 
-import java.util.Objects;
-
-public final class ContactData {
-    private final String name;
-    private final String last_name;
-    private final String address;
-    private final String email;
-    private final String mobile;
-
-    public ContactData(String name, String last_name, String address, String email, String mobile) {
-        this.name = name;
-        this.last_name = last_name;
-        this.address = address;
-        this.email = email;
-        this.mobile = mobile;
-    }
-
+public record ContactData(String name, String last_name, String address, String email, String mobile) {
     public ContactData() {
         this("", "", "", "", "");
-
     }
 
     public ContactData withName(String name) {
@@ -46,52 +29,4 @@ public final class ContactData {
         return new ContactData(this.name, last_name, this.address, this.email, mobile);
 
     }
-
-    public String name() {
-        return name;
-    }
-
-    public String last_name() {
-        return last_name;
-    }
-
-    public String address() {
-        return address;
-    }
-
-    public String email() {
-        return email;
-    }
-
-    public String mobile() {
-        return mobile;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == this) return true;
-        if (obj == null || obj.getClass() != this.getClass()) return false;
-        var that = (ContactData) obj;
-        return Objects.equals(this.name, that.name) &&
-                Objects.equals(this.last_name, that.last_name) &&
-                Objects.equals(this.address, that.address) &&
-                Objects.equals(this.email, that.email) &&
-                Objects.equals(this.mobile, that.mobile);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, last_name, address, email, mobile);
-    }
-
-    @Override
-    public String toString() {
-        return "ContactData[" +
-                "name=" + name + ", " +
-                "last_name=" + last_name + ", " +
-                "address=" + address + ", " +
-                "email=" + email + ", " +
-                "mobile=" + mobile + ']';
-    }
-
 }
