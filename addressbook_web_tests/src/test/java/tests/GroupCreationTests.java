1736c796f989868318f306b759ca4767a20b1ec1
Anvar <myanvar@yahoo.com>
2024-11-03 18:46:06 +0600

    Код после лекции 4.5. Параметризованные тесты

--- addressbook_web_tests/src/test/java/tests/GroupCreationTests.java
+++ addressbook_web_tests/src/test/java/tests/GroupCreationTests.java
@@ -1,50 +1,54 @@
 package tests;
 
 import model.GroupData;
-
-import java.util.ArrayList;
-import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GroupCreationTests  extends TestBase {
 
-    public static List<String> groupNameProvider(){
-        var result = new ArrayList<String>(List.of("group name", "group name'"));
+    public static List<GroupData> groupProvider(){
+        var result = new ArrayList<GroupData>();
+        for (var name : List.of("", "group name")) {
+            for (var header : List.of("", "group header")) {
+                for (var footer : List.of("", "group footer")) {
+                    result.add(new GroupData(name, header, footer));
+                }
+            }
+        }
+
+
         for (int i = 0; i < 5; i++) {
-            result.add(randomString(i * 10));
+            result.add(new GroupData(randomString(i * 10), randomString(i * 10), randomString(i * 10)));
         }
         return result;
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"group name", "group name '"})
-    public void canCreateGroup(String name) {
+    @MethodSource("groupProvider")
+    public void canCreateMultipleGroups(GroupData group) {
         int groupCount = app.groups().getCount();
-        app.groups().createGroup(new GroupData(name, "group header", "group footer"));
+        app.groups().createGroup(group);
         int newGroupCount = app.groups().getCount();
         Assertions.assertEquals(groupCount + 1, newGroupCount);
     }
 
-    @Test
-    public void canCreateGroupWithEmptyName() {
-        app.groups().createGroup(new GroupData());
-    }
-
-    @Test
-    public void canCreateGroupWithNameOnly() {
-        app.groups().createGroup(new GroupData().withName("some name"));
+    public static List<GroupData> negativeGroupProvider(){
+        var result = new ArrayList<GroupData>(List.of(
+                new GroupData("group name'", "", "")));
+        return result;
     }
 
     @ParameterizedTest
-    @MethodSource("groupNameProvider")
-    public void canCreateMultipleGroups(String name) {
+    @MethodSource("negativeGroupProvider")
+    public void canNotCreateMultipleGroups(GroupData group) {
         int groupCount = app.groups().getCount();
-        app.groups().createGroup(new GroupData(name, "group header", "group footer"));
+        app.groups().createGroup(group);
         int newGroupCount = app.groups().getCount();
-        Assertions.assertEquals(groupCount + 1, newGroupCount);
+        Assertions.assertEquals(groupCount, newGroupCount);
     }
 }
