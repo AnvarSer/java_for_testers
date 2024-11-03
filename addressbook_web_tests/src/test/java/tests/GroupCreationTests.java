0a2c6c7fda9475c6ccd64c7ae302abca55a79d05
Anvar <myanvar@yahoo.com>
2024-11-03 16:10:36 +0600

    Удаляю тест canCreateGroup на 08:30 минуте лекции 4.5. Параметризованные тесты

--- addressbook_web_tests/src/test/java/tests/GroupCreationTests.java
+++ addressbook_web_tests/src/test/java/tests/GroupCreationTests.java
@@ -1,15 +1,30 @@
 package tests;
 
 import model.GroupData;
+
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class GroupCreationTests  extends TestBase {
 
-    @Test
-    public void canCreateGroup() {
+    public static List<String> groupNameProvider(){
+        var result = new ArrayList<String>(List.of("group name", "group name'"));
+        for (int i = 0; i < 5; i++) {
+            result.add(randomString(i * 10));
+        }
+        return result;
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"group name", "group name '"})
+    public void canCreateGroup(String name) {
         int groupCount = app.groups().getCount();
-        app.groups().createGroup(new GroupData("group name", "group header", "group footer"));
+        app.groups().createGroup(new GroupData(name, "group header", "group footer"));
         int newGroupCount = app.groups().getCount();
         Assertions.assertEquals(groupCount + 1, newGroupCount);
     }
@@ -24,16 +39,12 @@ public class GroupCreationTests  extends TestBase {
         app.groups().createGroup(new GroupData().withName("some name"));
     }
 
-    @Test
-    public void canCreateMultipleGroups() {
-        int n = 5;
+    @ParameterizedTest
+    @MethodSource("groupNameProvider")
+    public void canCreateMultipleGroups(String name) {
         int groupCount = app.groups().getCount();
-
-        for (int i = 0; i < n; i++) {
-            app.groups().createGroup(new GroupData(randomString(i * 10), "group header", "group footer"));
-        }
-
+        app.groups().createGroup(new GroupData(name, "group header", "group footer"));
         int newGroupCount = app.groups().getCount();
-        Assertions.assertEquals(groupCount + n, newGroupCount);
+        Assertions.assertEquals(groupCount + 1, newGroupCount);
     }
 }
